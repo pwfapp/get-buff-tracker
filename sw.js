8dd37545de61b1c874a3eb46e07e8eb2cfc779a0
pwfapp <petewindy@gmail.com>
2026-09-06 18:45:55 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Minimal offline shell. Bump CACHE when you change index.html.
-const CACHE = 'gbt-v2';
+const CACHE = 'gbt-v3';
 const SHELL = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
